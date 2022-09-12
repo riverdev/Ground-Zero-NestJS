5e8dd0b1c036da1f97dff8af8deb91d3f504f411
riverdev <yariv.rivercybe@gmail.com>
2022-09-12 11:12:58 +0300

    feat(github readme): updated the github readme for the OpenSource project
    I marked this as a feature to verify that the schematic versioning is working

--- src/app.service.ts
+++ src/app.service.ts
@@ -16,7 +16,7 @@ export class AppService {
     console.log({ myEnvVarName });
 
     return `<h1> Ground-Zero-NestJS Open Source Project     </h1>
-            <p> Last updated: 2022-09-11-1927 branch [work] </p>
+            <p> Last updated: 2022-09-12-1110 branch [work] </p>
             <br> </br>
             <a href="https://github.com/riverdev/Ground-Zero-NestJS">Link to GitHub repo</a>
             <br> </br>
